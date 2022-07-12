9222b149d8f68718b1b40545cb16f4a3848639a7
kimhyonseong <corea6524@gmail.com>
2022-07-12 22:49:45 +0900

    proxy pattern

--- design/src/design/aop/AopBrowser.java
+++ design/src/design/aop/AopBrowser.java
@@ -9,9 +9,9 @@ public class AopBrowser implements IBrowser {
     private Runnable before;
     private Runnable after;
 
-    public AopBrowser(String url, Html html, Runnable after) {
+    public AopBrowser(String url, Runnable before, Runnable after) {
         this.url = url;
-        this.html = html;
+        this.before = before;
         this.after = after;
     }
 
@@ -21,14 +21,15 @@ public class AopBrowser implements IBrowser {
         if (html ==null) {
             this.html = new Html(url);
             System.out.println("AopBrowser html loading from "+url);
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        after.run();
         System.out.println("AopBrowser html cache "+url);
 
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         return null;
     }
 }
