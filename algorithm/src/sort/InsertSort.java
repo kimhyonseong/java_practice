644461c7ac5522f317bba78ad9f5f6feafbffd53
kimhyonseong <corea6524@gmail.com>
2022-09-28 15:58:47 +0900

    modify insertSort

--- algorithm/src/sort/InsertSort.java
+++ algorithm/src/sort/InsertSort.java
@@ -7,20 +7,43 @@ public class InsertSort {
         int tmp;
         int a;
 
+        /*
         for (int i=1; i< array.length; i++) {
-            a = i;
+//            a = i;
+//            while (a > 0) {
+//                if (array[a-1] > array[a]) {
+//                    tmp = array[a];
+//                    array[a] = array[a - 1];
+//                    array[a - 1] = tmp;
+//                }
+//                a--;
+//            }
 
-            while (a > 0) {
-                if (array[a-1] > array[a]) {
-                    tmp = array[a];
-                    array[a] = array[a - 1];
-                    array[a - 1] = tmp;
-                }
+            a = i-1;
+            int target = array[i];
+
+            while (a >= 0 && (target < array[a])) {
+                array[a+1] = array[a];
                 a--;
             }
+            array[a+1] = target;
             System.out.println(Arrays.toString(array));
-        }
 
+        }*/
+
+        int target;
+        int targetIndex;
+
+        for (int i=1; i<array.length; i++) {
+            target = array[i];
+            targetIndex = i-1;
+
+            while (targetIndex >= 0 && (target < array[targetIndex])) {
+                array[targetIndex+1] = array[targetIndex];
+                targetIndex--;
+            }
+            array[targetIndex+1] = target;
+        }
         return array;
     }
     public static void main(String[] args) {
---
+++ algorithm/src/sort/dijkstra.java
@@ -0,0 +1,7 @@
+package sort;
+
+public class dijkstra {
+    public static void main(String[] args) {
+
+    }
+}
