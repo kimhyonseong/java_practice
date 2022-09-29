555a215b1aaa6e47a34a2fa691a595a17862ac85
kimhyonseong <corea6524@gmail.com>
2022-09-29 17:27:11 +0900

    remove min num

--- codingTest/src/easy/removeMinNum.java
+++ codingTest/src/easy/removeMinNum.java
@@ -1,34 +1,31 @@
 package easy;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 class removeMinNumSolution {
     public int[] solution(int[] arr) {
         int[] answer = new int[arr.length-1];
 
-        int target = arr[0];
-        int targetIndex = 0;
+        if (answer.length == 0) {
+            answer = new int[]{-1};
+        } else {
+            int min = Arrays.stream(arr).sorted().toArray()[0];
+            int index = -1;
 
-        for(int i=1; i< arr.length; i++) {
-            if((arr[i-1] - arr[i]) < 0) {
-                targetIndex = i-1;
-                target = arr[i-1];
-            } else {
-                targetIndex = i;
-                target = arr[i];
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == min) index = i;
             }
-        }
 
-        System.out.println("targetIndex : "+targetIndex);
-        System.out.println("target : "+target);
+            System.out.println("min : "+min);
+            System.out.println("index : "+index);
 
-        for(int i=targetIndex; i< arr.length; i++) {
-            arr[i] = arr[i+1];
-        }
+            for (int i = index; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
 
-        for(int i=0; i<answer.length; i++) {
-            answer[i] = arr[i];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = arr[i];
+            }
         }
 
         return answer;
